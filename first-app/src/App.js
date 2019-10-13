6310cce2da7d2cc42f0ed96f287e475a53e8a040
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-13 13:48:24 +0100

    Add axios API

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 function App() {
   return (
     <div className="App">
-      We will start to learn React!
+      We will make us of axios API.
     </div>
   );
 }
